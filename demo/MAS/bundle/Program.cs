f28f229cc3d70e8fabe3147f490bbc107c08f2cd
unknown <peter@falkesand.se>
2026-03-04 22:51:12 +0100

    feat(demo/MAS): add bundle project chaining 5 MSI packages

---
+++ demo/MAS/bundle/Program.cs
@@ -0,0 +1,49 @@
+using FalkForge;
+using FalkForge.Compiler.Bundle.Builders;
+using FalkForge.Compiler.Bundle.Compilation;
+
+var packagesDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "packages"));
+
+string MsiPath(string name) => Path.Combine(packagesDir, name, "bin", "Release", $"{name}-8.9.0.msi");
+
+return Installer.BuildBundle(args, outputPath =>
+{
+    var bundle = new BundleBuilder()
+        .Name("MultiAccess Suite")
+        .Manufacturer("ASSA ABLOY")
+        .Version("8.9.0")
+        .BundleId(new Guid("10203040-5060-4070-8090-A0B0C0D0E0F0"))
+        .UpgradeCode(new Guid("F0E0D0C0-B0A0-4090-8070-605040302010"))
+        .Scope(InstallScope.PerMachine)
+        .UseCustomUI("../MAS.csproj")
+        .Chain(chain => chain
+            .MsiPackage(MsiPath("MultiAccess"), p => p
+                .Id("MultiAccess")
+                .DisplayName("MultiAccess")
+                .Version("8.9.0")
+                .Vital(true))
+            .MsiPackage(MsiPath("MultiServer"), p => p
+                .Id("MultiServer")
+                .DisplayName("MultiServer")
+                .Version("8.9.0")
+                .Vital(true))
+            .MsiPackage(MsiPath("MultiServerEx"), p => p
+                .Id("MultiServerEx")
+                .DisplayName("MultiServerEx")
+                .Version("8.9.0")
+                .Vital(true))
+            .MsiPackage(MsiPath("Konfigurera"), p => p
+                .Id("Konfigurera")
+                .DisplayName("Konfigurera")
+                .Version("8.9.0")
+                .Vital(true))
+            .MsiPackage(MsiPath("Concatenate"), p => p
+                .Id("Concatenate")
+                .DisplayName("Concatenate")
+                .Version("8.9.0")
+                .Vital(true)))
+        .Build();
+
+    var compiler = new BundleCompiler();
+    return compiler.Compile(bundle, outputPath);
+});
