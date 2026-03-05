fed9fd60eea744dc702509c6eaa95d1087fb91d8
unknown <peter@falkesand.se>
2026-03-05 14:53:48 +0100

    feat(protocol): add SearchCondition model types and DetectionMode

---
+++ src/FalkForge.Engine.Protocol/Manifest/SearchConditionType.cs
@@ -0,0 +1,10 @@
+namespace FalkForge.Engine.Protocol.Manifest;
+
+public enum SearchConditionType
+{
+    FileExists,
+    FileVersion,
+    DirectoryExists,
+    RegistryValue,
+    ProductSearch
+}
